82574b0aea076c4aa63ad86e88c918b340e1cbb0
jacoloves <5511068t@gmail.com>
2020-08-26 22:06:58 +0900

    2020826

--- src/bios-subroutine32/subroutine32.asm
+++ src/bios-subroutine32/subroutine32.asm
@@ -11,7 +11,7 @@ puts:
     cmp al, 0     ; 文字列の末尾
     je  puts_end  ; に来たら終了
     mov ah, 0x0e
-    mov ebx, 15
+    mov ebx, 10
     int 0x10      ; BIOS を呼び出す
     jmp puts
 puts_end:
@@ -19,4 +19,3 @@ puts_end:
 
 msg:
     db "hello, world", 0x0d, 0x0a, 0
-        
